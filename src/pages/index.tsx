d273cedd7f74e1051b03baec1bcb43839c4ea4d7
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2022-10-09 13:34:16 +0100

    Fullscreen Support (onClick)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+
+import { useFullscreen } from "rooks";
 import Head from "next/head";
 import type { NextPage } from "next";
 
@@ -6,13 +9,17 @@ import { ImageStack } from "@/components/ImageStack";
 
 const Home: NextPage = () => {
   const images = useImages();
+  const fullscreenContainerRef = useRef<HTMLDivElement>(null);
+  const { toggleFullscreen } = useFullscreen({
+    target: fullscreenContainerRef,
+  });
   return (
-    <>
+    <div ref={fullscreenContainerRef} onClick={toggleFullscreen}>
       <Head>
         <title>Phantom Diffusion</title>
       </Head>
       <ImageStack images={images} />
-    </>
+    </div>
   );
 };
 
