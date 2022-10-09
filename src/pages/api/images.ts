61f7abc6ffaf749230d25ee38c753e8acea28e03
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2022-10-10 00:23:32 +0100

    Fix broken images (#6)

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -9,11 +9,16 @@ type Data = {
   images: string[];
 };
 
+// Files appear to take a while to write... make sure they have existed for a little
+const MIN_DIFF = 2000;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const files = await fs.promises.readdir(DIR);
+
+  const now = new Date().getTime();
 
   const images = files
     .map((fileName) => {
@@ -27,6 +32,10 @@ export default async function handler(
       return a.time - b.time;
     })
     .filter((f) => path.extname(f.name).toLowerCase() === `.${FILE_EXTENSION}`)
+    .filter((f) => {
+      const diff = now - f.time;
+      return diff > MIN_DIFF;
+    })
     .reverse()
     .map((f) => {
       return f.name;
