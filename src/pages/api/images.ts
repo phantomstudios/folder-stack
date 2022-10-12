0f5947f5b2073b5f1872d246931e708e55d8c62d
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2022-10-13 00:17:16 +0100

    Recursive Folders (#11)

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -1,37 +1,44 @@
 import path from "path";
 import fs from "fs";
 
+import recursive from "recursive-readdir";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { DIR, FILE_EXTENSION, MAX_IMAGES } from "@/config";
+import { DIR, FILE_EXTENSION, MAX_IMAGES, RECURSIVE } from "@/config";
 
 type Data = {
   images: string[];
 };
 
 // Files appear to take a while to write... make sure they have existed for a little
-const MIN_DIFF = 2000;
+const MIN_DIFF = 2 * 1000;
 
 export default async function handler(
-  req: NextApiRequest,
+  _: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const files = await fs.promises.readdir(DIR);
+  const files = RECURSIVE
+    ? await recursive(DIR)
+    : (await fs.promises.readdir(DIR)).map((f) => path.join(DIR, f));
 
   const now = new Date().getTime();
 
   const images = files
     .map((fileName) => {
-      const stat = fs.statSync(DIR + "/" + fileName);
+      const stat = fs.statSync(fileName);
       return {
-        name: fileName,
+        name: path.relative(DIR, fileName),
         time: stat.mtime.getTime(),
       };
     })
     .sort((a, b) => {
       return a.time - b.time;
     })
-    .filter((f) => path.extname(f.name).toLowerCase() === `.${FILE_EXTENSION}`)
+    .filter(
+      (f) =>
+        path.extname(f.name).toLowerCase() ===
+        `.${FILE_EXTENSION}`.toLowerCase()
+    )
     .filter((f) => {
       const diff = now - f.time;
       return diff > MIN_DIFF;
