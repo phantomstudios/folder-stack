fbf9a4ce86b0cdabc2f95f77668cf339b4e436b3
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2022-10-14 07:32:58 +0100

    Fix image sizing

--- src/components/ImageStack/index.tsx
+++ src/components/ImageStack/index.tsx
@@ -1,17 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-import { CSSProperties } from "react";
-
 import { SHOW_TITLE } from "@/config";
 
 import styles from "./ImageStack.module.css";
 
 interface Props {
   images?: string[];
-  style?: CSSProperties;
 }
 
-export const ImageStack = ({ images = [], style }: Props) => (
-  <ul className={styles.list} style={style}>
+export const ImageStack = ({ images = [] }: Props) => (
+  <ul className={styles.list}>
     {images.map((image) => (
       <li key={image}>
         {SHOW_TITLE && <h2>{image}</h2>}
